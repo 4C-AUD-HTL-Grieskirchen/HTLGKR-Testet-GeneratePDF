541e4577c1a8d7c093d575766f1070196155e8f3
Michael Wiesinger <michw2014@gmail.com>
2021-04-18 12:24:24 +0200

    Added firebase authProvider

---
+++ PdfCreationDemo/PdfCreationDemo/Config.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace PdfCreationDemo
+{
+    public class Config
+    {
+        public string ApiKey { get; set; }
+        public string Email { get; set; }
+        public string Pw { get; set; }
+
+        public static Config Read()
+        {
+            using var stream = new FileStream("config.xml", FileMode.Open);
+            return new XmlSerializer(typeof(Config)).Deserialize(stream) as Config;
+        }
+
+        public void Save()
+        {
+            using var stream = new FileStream("config.xml", FileMode.OpenOrCreate);
+            new XmlSerializer(typeof(Config)).Serialize(stream, this);
+        }
+
+
+    }
+}
--- PdfCreationDemo/PdfCreationDemo/Program.cs
+++ PdfCreationDemo/PdfCreationDemo/Program.cs
@@ -1,78 +1,70 @@
-﻿using iText.Kernel.Pdf;
-using iText.Layout;
-using iTextSharp.text;
-using System;
-using System.Security.AccessControl;
-using iTextSharp.text.pdf;
-using PdfWriter = iTextSharp.text.pdf.PdfWriter;
-using Document = iTextSharp.text.Document;
-using System.IO;
-using System.Security.AccessControl;
-using iText.Layout.Properties;
-using iText.IO.Image;
-using PdfDocument = iText.Kernel.Pdf.PdfDocument;
+﻿using System;
 using System.Drawing.Imaging;
-
-using FireSharp.Config;
-using FireSharp.Interfaces;
-using FireSharp.Response;
+using System.IO;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Firebase.Auth;
 using Firebase.Storage;
 using FireSharp;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using FirebaseConfig = FireSharp.Config.FirebaseConfig;
 
-namespace PdfCreationDemo 
+namespace PdfCreationDemo
 {
     public class Program
     {
-        static void Main(string[] args)
+        private static string FIREBASE_APPKEY;
+
+        private static void Main(string[] args)
         {
-            Program p = new Program();
+            
+
+            var p = new Program();
             p.Program_Load();
             p.GeneratePDF();
-            p.pushFirebaseAsync();
-
+            p.PushFirebaseAsync(Config.Read()).Wait(10000);
         }
 
         public void Program_Load()
         {
-
             IFirebaseConfig config = new FirebaseConfig
             {
                 AuthSecret = "AIzaSyB04YQ9Xv7ZHyMjUNvu3iRwRm-rxrXXMsg",
-                BasePath = "https://htlgkr-testet-default-rtdb.firebaseio.com",
-
+                BasePath = "https://htlgkr-testet-default-rtdb.firebaseio.com"
             };
 
             IFirebaseClient client;
 
-            client = new FireSharp.FirebaseClient(config);
+            client = new FirebaseClient(config);
         }
 
         public void GeneratePDF()
         {
-            
-            var path = @"C:\Users\Julia\Desktop\AUD-PDFCreation\PdfCreation\PdfCreationDemo\pdfcreated.pdf";
+            var path = @"pdfcreated.pdf";
 
-            
-            Document pdfDocument = new Document();
+
+            var pdfDocument = new Document();
 
             PdfWriter.GetInstance(pdfDocument,
-                    new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.None));
+                new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.None));
             pdfDocument.Open();
 
             //Logo
-            var imagepathLogo = @"C:\Users\Julia\Desktop\AUD-PDFCreation\PdfCreation\Bilder\logo.png";
-            using(FileStream fs = new FileStream(imagepathLogo, FileMode.Open))
+            var imagepathLogo = @"Bilder\logo.png";
+            using (var fs = new FileStream(imagepathLogo, FileMode.Open))
             {
                 var png = Image.GetInstance(System.Drawing.Image.FromStream(fs),
                     ImageFormat.Png);
                 png.ScalePercent(8f);
-                
+
                 png.SetAbsolutePosition(pdfDocument.Left, pdfDocument.Top);
                 pdfDocument.Add(png);
-
             }
 
-            using (FileStream fs = new FileStream(imagepathLogo, FileMode.Open))
+            using (var fs = new FileStream(imagepathLogo, FileMode.Open))
             {
                 var png = Image.GetInstance(System.Drawing.Image.FromStream(fs),
                     ImageFormat.Png);
@@ -80,12 +72,11 @@ namespace PdfCreationDemo
 
                 png.SetAbsolutePosition(pdfDocument.Right, pdfDocument.Top);
                 pdfDocument.Add(png);
-
             }
 
             //TopText
-            iTextSharp.text.Font myText = FontFactory.GetFont("Arial", 18, iTextSharp.text.Font.BOLD);
-            Paragraph TopText = new Paragraph("Aktion Österreich testet", myText);
+            var myText = FontFactory.GetFont("Arial", 18, Font.BOLD);
+            var TopText = new Paragraph("Aktion Österreich testet", myText);
             pdfDocument.Add(TopText);
             pdfDocument.Add(new Paragraph("Einwilligungserklärung Antigen-Test", myText));
             pdfDocument.Add(new Paragraph("SARS-CoV-2 / Covid-19", myText));
@@ -97,48 +88,48 @@ namespace PdfCreationDemo
 
 
             //Table
-            PdfPTable table = new PdfPTable(4);
-            PdfPCell cell = new PdfPCell();
+            var table = new PdfPTable(4);
+            var cell = new PdfPCell();
             cell.Colspan = 4;
             cell.HorizontalAlignment = 1;
             table.AddCell(cell);
 
-            table.AddCell("Name:        ");//table.AddCell("Col 1 Row 1");
-            table.AddCell("             ");//table.AddCell("Col 2 Row 1");
+            table.AddCell("Name:        "); //table.AddCell("Col 1 Row 1");
+            table.AddCell("             "); //table.AddCell("Col 2 Row 1");
 
-            table.AddCell("Geburtsdatum: ");//table.AddCell("Col 3 Row 1");
-            table.AddCell("             ");//table.AddCell("Col 1 Row 2");
+            table.AddCell("Geburtsdatum: "); //table.AddCell("Col 3 Row 1");
+            table.AddCell("             "); //table.AddCell("Col 1 Row 2");
 
-            table.AddCell("             ");//table.AddCell("Col 2 Row 2");
-            table.AddCell("             ");//table.AddCell("Col 3 Row 2");
+            table.AddCell("             "); //table.AddCell("Col 2 Row 2");
+            table.AddCell("             "); //table.AddCell("Col 3 Row 2");
 
-            table.AddCell("Geschlecht:  ");//table.AddCell("Col 3 Row 2");
-            table.AddCell("             ");//table.AddCell("Col 3 Row 2");
+            table.AddCell("Geschlecht:  "); //table.AddCell("Col 3 Row 2");
+            table.AddCell("             "); //table.AddCell("Col 3 Row 2");
 
-            table.AddCell("Vorname      ");//table.AddCell("Col 3 Row 2");
-            table.AddCell("             ");//table.AddCell("Col 3 Row 2");
+            table.AddCell("Vorname      "); //table.AddCell("Col 3 Row 2");
+            table.AddCell("             "); //table.AddCell("Col 3 Row 2");
 
-            table.AddCell("SV-Nummer(10-stellig): ");//table.AddCell("Col 3 Row 2");
-            table.AddCell("             ");//table.AddCell("Col 3 Row 2");
+            table.AddCell("SV-Nummer(10-stellig): "); //table.AddCell("Col 3 Row 2");
+            table.AddCell("             "); //table.AddCell("Col 3 Row 2");
 
-            table.AddCell("PLZ, Ort:    ");//table.AddCell("Col 3 Row 2");
-            table.AddCell("             ");//table.AddCell("Col 3 Row 2");
+            table.AddCell("PLZ, Ort:    "); //table.AddCell("Col 3 Row 2");
+            table.AddCell("             "); //table.AddCell("Col 3 Row 2");
 
-            table.AddCell("Mobiltelefonnummer:    ");//table.AddCell("Col 3 Row 2");
-            table.AddCell("             ");//table.AddCell("Col 3 Row 2");
+            table.AddCell("Mobiltelefonnummer:    "); //table.AddCell("Col 3 Row 2");
+            table.AddCell("             "); //table.AddCell("Col 3 Row 2");
 
-            table.AddCell("Straße, Hausnummer:");//table.AddCell("Col 3 Row 2");
-            table.AddCell("             ");//table.AddCell("Col 3 Row 2");
+            table.AddCell("Straße, Hausnummer:"); //table.AddCell("Col 3 Row 2");
+            table.AddCell("             "); //table.AddCell("Col 3 Row 2");
 
-            table.AddCell("E-Mail-Adresse:");//table.AddCell("Col 3 Row 2");
-            table.AddCell("             ");//table.AddCell("Col 3 Row 2");
+            table.AddCell("E-Mail-Adresse:"); //table.AddCell("Col 3 Row 2");
+            table.AddCell("             "); //table.AddCell("Col 3 Row 2");
 
             pdfDocument.Add(table);
 
 
             //TestergebnisBottom
-            var imagepathTEB = @"C:\Users\Julia\Desktop\AUD-PDFCreation\PdfCreation\Bilder\TestergebnisFoto.png";
-            using (FileStream fs = new FileStream(imagepathTEB, FileMode.Open))
+            var imagepathTEB = @"Bilder\TestergebnisFoto.png";
+            using (var fs = new FileStream(imagepathTEB, FileMode.Open))
             {
                 var png = Image.GetInstance(System.Drawing.Image.FromStream(fs),
                     ImageFormat.Png);
@@ -146,23 +137,28 @@ namespace PdfCreationDemo
 
                 png.SetAbsolutePosition(pdfDocument.Bottom, pdfDocument.Bottom);
                 pdfDocument.Add(png);
-
             }
 
-            pdfDocument.Close(); 
-
-
-
+            pdfDocument.Close();
         }
 
-        public async System.Threading.Tasks.Task pushFirebaseAsync()
+        public async Task PushFirebaseAsync(Config conf)
         {
+            Console.WriteLine("Start upload ...");
             // Get any Stream - it can be FileStream, MemoryStream or any other type of Stream
-            var stream = File.Open(@"C:\Users\Julia\Desktop\AUD-PDFCreation\PdfCreation\PdfCreationDemo\pdfcreated.pdf", FileMode.Open);
+            var stream = File.Open(@"pdfcreated.pdf", FileMode.Open);
+
+            var authProvider =
+                new FirebaseAuthProvider(new Firebase.Auth.FirebaseConfig(conf.ApiKey));
+            var auth = await authProvider.SignInWithEmailAndPasswordAsync(conf.Email, conf.Pw);
+            var authOption = new FirebaseStorageOptions
+            {
+                AuthTokenAsyncFactory = () => Task.FromResult(auth.FirebaseToken),
+                ThrowOnCancel = true
+            };
 
             // Construct FirebaseStorage, path to where you want to upload the file and Put it there
-            var task = new FirebaseStorage(@"pdf-creation-56da4.appspot.com")
-                .Child("data")
+            var task = new FirebaseStorage(@"htlgkr-testet.appspot.com", authOption)
                 .Child("pdf")
                 .Child("pdfcreated.pdf")
                 .PutAsync(stream);
@@ -173,6 +169,7 @@ namespace PdfCreationDemo
             // await the task to wait until upload completes and get the download url
             var downloadUrl = await task;
 
+            Console.WriteLine("URL: " + downloadUrl);
         }
     }
 }
